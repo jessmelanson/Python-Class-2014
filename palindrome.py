69c48cae00aa2fae9104c2b8028f170470854e77
jessmelanson <jessica_melanson@yahoo.com>
2014-11-04 20:03:13 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,12 @@
+#open the document
 f = open('words.txt')
-
+#make a list with each list element being one line of the file
 for line in f.readlines():
-    if((line.replace(" ", "").strip('\n')[::] == line.replace(" ","").strip('\n')[::-1]) == True):
+    #first remove all spaces from the line
+    #then remove the new line code \n from each line
+    #then check if the word forwards is equivalent to the word backwords
+    if(line.replace(" ", "").strip('\n')[::] == line.replace(" ","").strip('\n')[::-1]):
+        #if the line is the same forwards and backwards, print the line in the output
         print line
+#close the document
+f.close()
