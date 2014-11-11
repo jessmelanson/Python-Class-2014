23de20a03e89e8f99a9523c6ef432aa5f00d157b
jessmelanson <jessica_melanson@yahoo.com>
2014-11-11 17:09:24 -0500

    Update rot13v2.py

--- rot13v2.py
+++ rot13v2.py
@@ -3,3 +3,9 @@ cipherCodes = dict()
 def addtocipherCodes(word):
     for letter in word:
         cipherCodes[letter] = ord(letter)
+        if ord(letter) > 96 and ord(letter) < 110:
+            cipherCodes[chr(ord(letter) + 13)] = ord(letter) + 13
+        else ord(letter) > 109 and ord(letter) < 123:
+            cipherCodes[chr(ord(letter) - 13)] = ord(letter) - 13
+        else:
+            print "Invalid entry. Please do not use spaces or special characters."
