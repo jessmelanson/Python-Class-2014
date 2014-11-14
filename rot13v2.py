3a8b6e79c15d203de1774a66b7309f61462580a8
jessmelanson <jessica_melanson@yahoo.com>
2014-11-13 22:12:58 -0500

    Update rot13v2.py

--- rot13v2.py
+++ rot13v2.py
@@ -1,34 +1,30 @@
-cipherCodes = dict()
+Ciphers = dict()
 
-def addtocipherCodes(word):
+def addtoCiphers(word):
     for letter in word:
         if ord(letter) > 96 and ord(letter) < 110:
-            cipherCodes[letter] = chr(ord(letter) + 13)
+            Ciphers[letter] = chr(ord(letter) + 13)
         elif ord(letter) > 109 and ord(letter) < 123:
-            cipherCodes[letter] = chr(ord(letter) - 13)
+            Ciphers[letter] = chr(ord(letter) - 13)
         elif ord(letter) > 64 and ord(letter) < 79:
-            cipherCodes[letter] = chr(ord(letter) + 13)
+            Ciphers[letter] = chr(ord(letter) + 13)
         elif ord(letter) > 78 and ord(letter) < 91:
-            cipherCodes[letter] = chr(ord(letter) - 13)
+            Ciphers[letter] = chr(ord(letter) - 13)
         else:
             print "Invalid entry. Please do not use spaces or special characters."
 
-def ciphenate(word):
-    addtocipherCodes(word)
+def encrypt(word):
+    addtoCiphers(word)
     for letter in word:
         return cipherCodes[letter]
 
 decodedCiphers = dict()
 
 def addtodecodedCiphers:
-    for key in cipherCodes:
-        decodedCiphers[cipherCodes[key]] = key
+    for key in Ciphers:
+        decodedCiphers[Ciphers[key]] = key
 
-def unciphenate(cipherWord):
-    addtodecodedCiphers(cipherWord)
-    for letter in cipherWord:
+def decrypt(codeword):
+    addtodecodedCiphers(codeword)
+    for letter in codeword:
         return decodedCiphers[letter]
-
-def encryptDecrypt(word):
-    print "Encrypted word: " + ciphenate(word)
-    print "Decrypted word: " + unciphenate(ciphenate(word))
